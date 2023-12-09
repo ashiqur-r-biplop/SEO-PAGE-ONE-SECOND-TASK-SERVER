e9375fa6e3cd7c911ee0d1aed26b046c0b71cc2f
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-12-09 19:55:03 +0600

    install multer and using multiple file upload apies:

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
@@ -19,6 +23,18 @@ const client = new MongoClient(uri, {
   },
 });
 
+// Multer configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads"); // specify the destination folder for your uploads
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // generate a unique filename
+  },
+});
+
+const upload = multer({ storage: storage });
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -26,16 +42,38 @@ async function run() {
     // Send a ping to confirm a successful connection
     const card = client.db("SEO-page-one-task").collection("card");
 
-    app.get("/", async (req, res) => {
-      res.send("SEO page 1 connected");
+    app.post("/upload/:clientId", upload.array("uploads"), async (req, res) => {
+      try {
+        const clientId = req.params.clientId;
+        const uploads = req.files;
+
+        // Save file information to the database (you may need to modify this part)
+        // Example: Update the 'uploads' field in the 'card' collection
+        await card.updateOne(
+          { "incomplete.client_id": clientId },
+          { $set: { "incomplete.$.uploads": uploads } }
+        );
+
+        // Remove uploaded files from the 'uploads' folder
+        uploads.forEach((file) => {
+          fs.unlinkSync(file.path);
+        });
+
+        res.status(200).send("Files uploaded successfully!");
+      } catch (error) {
+        console.log(error?.message);
+        res.status(500).send("Internal Server Error");
+      }
     });
-    app.get("/cards", async (req, res) => {
-      const cards = await card.find().toArray();
-      res.send(cards);
-    });
-    app.get("/cards", async (req, res) => {
-      const cards = await card.find().toArray();
-      res.send(cards);
+
+    app.get("/card/:cardType", async (req, res) => {
+      try {
+        const cardType = req?.params?.cardType;
+        const cards = await card.find({ type: cardType }).toArray();
+        res.send(cards);
+      } catch (error) {
+        console.log(error?.message);
+      }
     });
 
     await client.db("admin").command({ ping: 1 });
@@ -48,6 +86,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("SEO page 1 connected");
+});
 
 app.listen(port, () => {
   console.log("SEO page server is running");
